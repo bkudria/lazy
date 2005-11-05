9425ce3aedf5c42ca3d86b66fb745a21590beb67
MenTaLguY <mental@cato.moonbase>
2005-11-05 13:19:59 -0500

    Do a better job of proxying, and improve exception handling.

--- lazy.rb
+++ lazy.rb
@@ -7,28 +7,73 @@
 # You may redistribute it and/or modify it under the same terms as Ruby.
 #
 
-require 'delegate'
+module Lazy
 
-module Lazy #:nodoc: all
+# Raised when a forced computation diverges (e.g. if it tries to force its
+# own result or throws an exception)
+class DivergenceError < Exception
+  # the exception, if any, that caused the divergence
+  attr_reader :reason
 
-class Thunk < SimpleDelegator
+  def initialize( reason=nil )
+    @reason = reason
+    message = "Computation diverges"
+    if reason
+      set_backtrace( reason.backtrace )
+      message = "#{ message }: #{ reason } (#{ reason.class })"
+    end
+    super( message )
+  end
+end
+
+class Thunk #:nodoc: all
+  DIVERGES = lambda { raise DivergenceError::new }
+  instance_methods.each { |m| undef_method m unless m =~ /^__/ }
   def initialize( &computation )
     @computation = computation
-    super self
   end
-  def __getobj__
+  def __force__
     if @computation
-      result = force( @computation.call( self ) )
-      initialize_methods result
-      __setobj__ result
-      @computation = nil
+      # clone to maintain clean backtrace
+      raise @exception.clone if @exception
+
+      computation = @computation
+      @computation = DIVERGES # trap divergences due to recursion
+
+      begin
+        @result = force( computation.call( self ) )
+        @computation = nil
+      rescue Exception => exception
+        # handle divergences due to exceptions
+        if exception.kind_of? DivergenceError
+          @exception = exception
+        else
+          @exception = DivergenceError::new exception
+        end
+        raise @exception.clone
+      end
     end
-    super
+    @result
   end
   def method_missing( *args, &block )
-    __getobj__.send( *args, &block )
+    __force__.send( *args, &block )
   end
-  alias __force__ __getobj__
+  def respond_to?( message )
+    message = message.to_sym
+    if message == :__force__
+      true
+    else
+      __force__.respond_to? message
+    end
+  end
+end
+
+class Stream
+  attr_reader :head, :rest
+  def initialize( head, rest )
+    @head = head
+    @rest = rest
+  end
 end
 
 end
@@ -37,14 +82,16 @@ module Kernel
 
 # The promise() function is used together with force() to implement
 # lazy evaluation.  It returns a promise to evaluate the provided
-# block at a future time, which can be demanded by force().
+# block at a future time.  Evaluation can be forced and the block's
+# result obtained via the force() function.
 #
-# The promise can also (usually) be used directly in place of the final
-# result object; messages sent to the promise will force evaluation of
-# the block (if required) and be forwarded to the result object.
+# Implicit evaluation is also supported: a promise can usually be used
+# as a proxy for the result; the first message sent to it will force
+# evaluation, and that message and any subsequent messages will be
+# forwarded to the result object.
 #
-# As an aid to circular programming, the block will be passed its own
-# result.
+# As an aid to circular programming, the block will be passed a promise
+# for its own result when it is evaluated.
 #
 def promise( &computation )
   Lazy::Thunk::new &computation
