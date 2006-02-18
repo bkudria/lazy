e1403383bbc89448c4684619970c71c5b695f9a3
MenTaLguY <mental@cato.moonbase>
2006-02-18 17:16:59 -0500

    Make locking faster; use a spinlock instead of (essentially) a mutex, and introduce a fast path for already evaluated thunks.

--- lazy/threadsafe.rb
+++ lazy/threadsafe.rb
@@ -3,9 +3,6 @@
 # Author:: MenTaLguY
 #
 # Copyright 2006  MenTaLguY <mental@rydia.net>
-# Copyright (C) 2001  Yukihiro Matsumoto
-# Copyright (C) 2000  Network Applied Communication Laboratory, Inc.
-# Copyright (C) 2000  Information-technology Promotion Agency, Japan
 #
 # You may redistribute it and/or modify it under the same terms as Ruby.
 #
@@ -16,40 +13,31 @@ require 'thread'
 module Lazy
 
 class Promise
-  def __init_lock__ #:nodoc:
-    @waiting ||= []
-  end
-
   def __synchronize__ #:nodoc:
     current = Thread.current
 
-    while (Thread.critical = true; @locked)
-      if @locked == current
-        Thread.critical = false
-        DIVERGES.call
-      else
-        @waiting.push current
-        Thread.stop
-      end
-    end
-    @locked = current
-    Thread.critical = false
-
-    begin
-      yield
-    ensure
-      Thread.critical = true
-      @locked = nil
-      begin
-        t = @waiting.shift
-        t.wakeup if t
-      rescue ThreadError
-        retry
-      end
+    Thread.critical = true
+    unless @computation
+      # fast path for evaluated thunks
       Thread.critical = false
+      yield
+    else
+      if @owner == current
+        Thread.critical = false
+        raise DivergenceError.new
+      end 
+      while @owner # spinlock
+        Thread.critical = false
+        Thread.pass
+        Thread.critical = true
+      end
+      @owner = current
+      Thread.critical = false
+
       begin
-        t.run if t
-      rescue ThreadError
+        yield
+      ensure
+        @owner = nil
       end
     end
   end
