5263aeba40d7c56feddc4dc94a720977bed80130
MenTaLguY <mental@rydia.net>
2006-12-16 17:50:32 -0500

    move futures into main lazy.rb

--- lib/lazy.rb
+++ lib/lazy.rb
@@ -123,6 +123,28 @@ class Promise
   end
 end
 
+Promise.ancestors.each do |c|
+class << c
+  alias lazy_rb_method_added method_added
+    def method_added( name )
+      lazy_rb_method_added( name )
+      return unless Lazy::Promise < self
+    end
+  end
+end
+
+# A promise whose computation runs asynchronously in the background.
+#
+class Future < Promise
+  def initialize( scheduler=Thread, &computation ) #:nodoc:
+    task = scheduler.new { computation.call self }
+    super() do
+      raise DivergenceError if Thread.current == thread
+      task.value
+    end
+  end
+end
+
 module Methods
 private
 
@@ -158,21 +180,30 @@ def force( promise )
   end
 end
 
+# Schedules a computation to be run asynchronously and returns a promise
+# for its result.  An attempt to force the result of the promise will
+# block until the computation finishes.
+#
+# If +scheduler+ is not specified, the computation is run in a background
+# thread which is joined when the result is forced.  A scheduler should
+# provide a method, new, which takes a block and returns a task
+# object.  The task object should provide a method, value, which awaits
+# completion of the task, returning its result or raising the exception that
+# terminated the task.  The Thread class trivially satisfies this
+# protocol.
+#
+# As with Lazy::Methods#force, this passes the block a promise for its own
+# result.  Use wisely.
+#
+def async( scheduler=Thread, &computation ) #:yields: own_result
+  Lazy::Future.new scheduler, &computation
+end 
+
 end
 
 extend Methods
 class << self
-  public :lazy, :force
-end
-
-Promise.ancestors.each do |c|
-  class << c
-    alias lazy_rb_method_added method_added
-    def method_added( name )
-      lazy_rb_method_added( name )
-      return unless Lazy::Promise < self
-    end
-  end
+public :lazy, :force, :async
 end
 
 end
